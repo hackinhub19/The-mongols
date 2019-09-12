9b10601ac69458475e4b2d2142750f912f641fbb
monishvar <monishvarramachandran@gmail.com>
2019-09-13 03:32:02 +0530

    AES algorithm for key encryption

---
+++ encryption.cs
@@ -0,0 +1,151 @@
+main frame for the encryption
+
+"
+using System.Collections.Generic;
+using System.Linq;
+using using System;
+System.Windows.Forms;
+
+namespace EncryptionAlgorithms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
+"
+
+AES Algorithm
+
+"
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace EncryptionAlgorithms
+{
+ public class AESEngine
+    {
+        
+        public static string Encrypt(string plainText,
+                                     string passPhrase,
+                                     string saltValue,
+                                     string hashAlgorithm,
+                                     int passwordIterations,
+                                     string initVector,
+                                     int keySize)
+        {
+            
+            PasswordDeriveBytes password = new PasswordDeriveBytes(                                                            passPhrase,
+                                                            saltValueBytes,
+                                                            hashAlgorithm,
+                                                            passwordIterations);
+
+            // Use the password to generate pseudo-random bytes for the encryption
+            // key. Specify the size of the key in bytes (instead of bits).
+            byte[] keyBytes = password.GetBytes(keySize / 8);
+
+            // Create uninitialized Rijndael encryption object.
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+
+            // It is reasonable to set encryption mode to Cipher Block Chaining
+            // (CBC). Use default options for other symmetric key parameters.
+            symmetricKey.Mode = CipherMode.CBC;
+
+            // Generate encryptor from the existing key bytes and initialization 
+            // vector. Key size will be defined based on the number of the key 
+            // bytes.
+            ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes,initVectorBytes);
+
+            // Define memory stream which will be used to hold encrypted data.
+            MemoryStream memoryStream = new MemoryStream();
+
+            // Define cryptographic stream (always use Write mode for encryption).
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,encryptor,CryptoStreamMode.Write);
+            
+            byte[] cipherTextBytes = memoryStream.ToArray();
+
+            // Close both streams.
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            // Convert encrypted data into a base64-encoded string.
+            string cipherText = Convert.ToBase64String(cipherTextBytes);
+
+            // Return encrypted string.
+            return cipherText;
+        }
+
+        
+        public static string Decrypt(string cipherText,
+                                     string passPhrase,
+                                     string saltValue,
+                                     string hashAlgorithm,
+                                     int passwordIterations,
+                                     string initVector,
+                                     int keySize)
+        {
+            
+
+
+
+            PasswordDeriveBytes password = new PasswordDeriveBytes(
+                                                            passPhrase,
+                                                            saltValueBytes,
+                                                            hashAlgorithm,
+                                                            passwordIterations);
+
+            
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor(
+                                                             keyBytes,
+                                                             initVectorBytes);
+
+            // Define memory stream which will be used to hold encrypted data.
+            MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+
+            // Define cryptographic stream (always use Read mode for encryption).
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                                                          decryptor,
+                                                          CryptoStreamMode.Read);
+
+            // Since at this point we don't know what the size of decrypted data
+            // will be, allocate the buffer long enough to hold ciphertext;
+            // plaintext is never longer than ciphertext.
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            // Start decrypting.
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes,
+                                                       0,
+                                                       plainTextBytes.Length);
+
+            // Close both streams.
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            // Convert decrypted data into a string. 
+            // Let us assume that the original plaintext string was UTF8-encoded.
+            string plainText = Encoding.UTF8.GetString(plainTextBytes,
+                                                       0,
+                                                       decryptedByteCount);
+
+            // Return decrypted string.   
+            return plainText;
+        }
+
+    }
+}
+
+
+"
